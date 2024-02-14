180f9478a86de7ef288249431ddb5132e7c5d4c3
Rop Gonggrijp <rop@gonggri.jp>
2024-02-14 17:17:32 +0100

    Update minimal_demo.ino

--- examples/minimal_demo/minimal_demo.ino
+++ examples/minimal_demo/minimal_demo.ino
@@ -1,6 +1,6 @@
 /*
     This will allow you to accs the Command Line Interpreter from the Serial
-    Monitor, letteing you set up a radio and receive and transmit packets.
+    Monitor, letting you set up a radio and receive and transmit packets.
 */
 
 #include <OOKwiz.h>
