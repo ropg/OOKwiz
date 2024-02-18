712d278ed41b9557b00bd6209b2d0d5044bc86c8
Rop Gonggrijp <rop@gonggri.jp>
2024-02-18 14:51:21 +0100

    typo fix

--- examples/minimal_demo/minimal_demo.ino
+++ examples/minimal_demo/minimal_demo.ino
@@ -1,5 +1,5 @@
 /*
-    This will allow you to accs the Command Line Interpreter from the Serial
+    This will allow you to access the Command Line Interpreter from the Serial
     Monitor, letting you set up a radio and receive and transmit packets.
 */
 
